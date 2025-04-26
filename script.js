25bfd8657fcd9b18a53e0cd884f34bf2ced8e0f4
godless <65888894+XLEBYSEK@users.noreply.github.com>
2025-04-26 19:49:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("btn").addEventListener("click",
+    function () {
+        const LoginInput = document.getElementById("log").value
+        const PasswordInput = document.getElementById("pass").value
+        let login = "log"
+        let password = "pass"
+        if (LoginInput === login && PasswordInput === password) {
+            window.open('index.html')
+
+        }
+    
+    else {
+        alert("Неверные данные")}
+    })
